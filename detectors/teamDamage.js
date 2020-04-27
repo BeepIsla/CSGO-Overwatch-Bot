b888012036ab89a095ba5a67b5a9bf0bfcfd7049
Isla <BeepIsla@gmail.com>
2020-04-27 12:16:24 +0200

    Fix error if victim doesn't exist

--- detectors/teamDamage.js
+++ detectors/teamDamage.js
@@ -3,7 +3,7 @@ module.exports = (demoFile, sid, data) => {
     demoFile.gameEvents.on("player_hurt", (event) => {
         const victim = demoFile.entities.getByUserId(event.userid);
         const attacker = demoFile.entities.getByUserId(event.attacker);
-        if (!attacker || attacker.steam64Id === "BOT" || attacker.steam64Id !== sid.getSteamID64() || victim === attacker) {
+        if (!victim || !attacker || attacker.steam64Id === "BOT" || attacker.steam64Id !== sid.getSteamID64() || victim === attacker) {
             return;
         }
         if(attacker.teamNumber === victim.teamNumber){
