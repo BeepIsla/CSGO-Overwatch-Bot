0a7354f510fe8ed114f8e1b52ce8139f8e3cf066
Jeferson <jeferson.santos@ilog.com.br>
2020-05-03 17:15:50 -0300

    Update Teamkill
    Update team kill for molotov

--- detectors/teamkill.js
+++ detectors/teamkill.js
@@ -3,7 +3,7 @@ module.exports = (demoFile, sid, data) => {
         const victim = demoFile.entities.getByUserId(event.userid);
         const attacker = demoFile.entities.getByUserId(event.attacker);
         isNotMolotovInc = (event) => {
-            return event.weapon != "incgrenade" || event.weapon != "molotov";
+            return event.weapon != "incgrenade" && event.weapon != "molotov";
         }
         if (!victim || !attacker || attacker.steam64Id === "BOT" || attacker.steam64Id !== sid.getSteamID64() || victim === attacker) {
             return;
