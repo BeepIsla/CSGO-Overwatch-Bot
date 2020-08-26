c7a0e45311790990a72af6b1e34452b8f77023c7
Black_Yuzia <25010528+BlackYuzia@users.noreply.github.com>
2020-08-26 20:26:15 +0300

    Remove non used code

--- detectors/AA.js
+++ detectors/AA.js
@@ -1,7 +1,5 @@
 module.exports = (demoFile, sid, data) => {
-    let array = [];
-
-    const array2 = demoFile.on("tickend__", (tick) => {
+    demoFile.on("tickend__", (tick) => {
         let ourPlayer = demoFile.players[tick.player];
         if (typeof ourPlayer === "undefined") {
             return;
@@ -15,12 +13,10 @@ module.exports = (demoFile, sid, data) => {
             // ! Be aware. This detector is unstable. 
             // ! I don't sure by 100%, but if player just look at floor (0deg) and didn't kill anybody
             // ! this detector can say what he have AntiAim (AA)... need more tests!
-            // ? Source: https://www.unknowncheats.me/forum/counterstrike-global-offensive/208735-detecting-player-antiaim.html
+            // ? Idea: https://www.unknowncheats.me/forum/counterstrike-global-offensive/208735-detecting-player-antiaim.html
             if (lbyDelta > 40 && ourPlayer.eyeAngles.pitch === 0) {
-                console.log(m_flLowerBodyYawTarget, "X,Y,Z:", ourPlayer.eyeAngles.yaw, ourPlayer.eyeAngles.pitch, 0, lbyDelta);
-                return data.curcasetempdata.AA_infractions.push({ eyeAngles, lbyDelta });
+                data.curcasetempdata.AA_infractions.push({ eyeAngles, lbyDelta });
             };
-            return;
         }
     });
 }
