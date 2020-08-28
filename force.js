0f5a6c86421e3002e6513fd9ca0d8902f6401d56
Black_Yuzia <kronky77@gmail.com>
2020-08-28 19:53:39 +0300

    Merge branch 'master' of https://github.com/BeepIsla/CSGO-Overwatch-Bot into BeepIsla-master

--- force.js
+++ force.js
@@ -2,30 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const inquire = require("inquirer");
 const SteamID = require("steamid");
-<<<<<<< HEAD
-const path = require("path");
-const fs = require("fs");
-
-const Aimbot = require("./detectors/aimbot.js");
-const AFKing = require("./detectors/AFKing.js");
-const Wallhack = require("./detectors/wallhack.js");
-const TeamKill = require("./detectors/teamkill.js");
-const TeamDamage = require("./detectors/teamDamage.js");
-
-const config = require("./config.json");
-
-let data = {
-	curcasetempdata: {
-		aimbot_infractions: [],
-		AFKing_infractions: [],
-		Wallhack_infractions: [],
-		teamKill_infractions: [],
-		teamDamage_infractions: 0
-	}
-};
-
-getResponse().then((result) => {
-=======
 const Demo = require("./helpers/Demo.js");
 const config = require("./config.json");
 
@@ -60,7 +36,6 @@ new Promise((resolve, reject) => {
 		});
 	}).catch(reject);
 }).then((result) => {
->>>>>>> 48ffed3427bacb17ec69ebff96f82016804d351a
 	if (result.path.trim().length <= 0) {
 		console.log("No path entered");
 		return;
@@ -95,34 +70,7 @@ new Promise((resolve, reject) => {
 			return;
 		}
 
-<<<<<<< HEAD
-		console.log("Parsing demo " + result.path.trim() + " with suspect " + sid.getSteamID64());
-
-		let lastProg = -1;
-		let playerIndex = -1;
-		const demoFile = new demofile.DemoFile();
-
-		demoFile.gameEvents.on("player_connect", getPlayerIndex);
-		demoFile.gameEvents.on("player_disconnect", getPlayerIndex);
-		demoFile.gameEvents.on("round_freeze_end", getPlayerIndex);
-
-		function getPlayerIndex() {
-			playerIndex = demoFile.players.filter(p => p.userInfo !== null).map(p => p.steamId === "BOT" ? p.steamId : new SteamID(p.steamId).getSteamID64()).indexOf(sid.getSteamID64());
-		}
-
-		demoFile.on("tickend", (curTick) => {
-			demoFile.emit("tickend__", { curTick: curTick, player: playerIndex });
-		});
-
-		// Detection
-		Aimbot(demoFile, sid, data, config);
-		AFKing(demoFile, sid, data, config);
-		Wallhack(demoFile, sid, data, config);
-		TeamKill(demoFile, sid, data);
-		TeamDamage(demoFile, sid, data);
-=======
 		console.log("Parsing demo with suspect " + sid.getSteamID64());
->>>>>>> 48ffed3427bacb17ec69ebff96f82016804d351a
 
 		let demo = new Demo(buffer, sid.getSteamID64(), config);
 		let lastVal = 0;
@@ -155,60 +103,8 @@ new Promise((resolve, reject) => {
 		}).catch((err) => {
 			process.stdout.write("\n");
 
-<<<<<<< HEAD
-			console.log("Suspect: " + (sid ? sid.getSteamID64() : 0));
-			console.log("Infractions:");
-			console.log("	Aimbot: " + data.curcasetempdata.aimbot_infractions.length);
-			console.log("	Wallhack: " + data.curcasetempdata.Wallhack_infractions.length);
-			console.log("	TeamKills: "+ data.curcasetempdata.teamKill_infractions.length);
-			console.log("	TeamDamage: " + data.curcasetempdata.teamDamage_infractions);
-			console.log("	Other: 0");
-			console.log("	Griefing: " + data.curcasetempdata.AFKing_infractions.length);
-		});
-	});
-});
-
-// Stuff for easier debugging in Visual Studio Code
-function getResponse() {
-	const args = process.argv; 
-
-	if (isDebugging() === true) {
-		return new Promise((resolve, reject) => {
-			resolve({
-				path: "rage.dem",
-				suspect: "76561198976843261"
-			});
-		});
-	} else if (args[2] && args[3]) {
-		return Promise.resolve({
-			path: args[2],
-			suspect: args[3]
-		});
-	} else {
-		return inquire.prompt([
-			{
-				type: "input",
-				name: "path",
-				message: "Enter the name of the demo:"
-			},
-			{
-				type: "input",
-				name: "suspect",
-				message: "Suspect SteamID:"
-			}
-		]);
-	}
-}
-
-function isDebugging() {
-	// Doesnt detect if a debugger is attached after launch but I do not need that for Visual Studio Code
-	const argv = process.execArgv.join();
-	return argv.includes("inspect-brk") || argv.includes("debug");
-}
-=======
 			// Fail parsing - What do?
 			console.error(err);
 		});
 	});
 });
->>>>>>> 48ffed3427bacb17ec69ebff96f82016804d351a
--- index.js
+++ index.js
@@ -4,17 +4,7 @@ const Stream = require("stream");
 const SteamUser = require("steam-user");
 const SteamTotp = require("steam-totp");
 const SteamID = require("steamid");
-<<<<<<< HEAD
-
-const Aimbot = require("./detectors/aimbot.js");
-const AFKing = require("./detectors/AFKing.js");
-const Wallhack = require("./detectors/wallhack.js");
-const TeamKill = require("./detectors/teamkill.js");
-const TeamDamage = require("./detectors/teamDamage.js");
-
-=======
 const unbzip2 = require("unbzip2-stream");
->>>>>>> 48ffed3427bacb17ec69ebff96f82016804d351a
 const Helper = require("./helpers/Helper.js");
 const Coordinator = require("./helpers/Coordinator.js");
 const Protobufs = require("./helpers/Protobufs.js");
@@ -25,44 +15,6 @@ const config = require("./config.json");
 const steam = new SteamUser({
 	autoRelogin: false // Do not automatically log back in - Process must exit and an external program like PM2 must auto restart
 });
-<<<<<<< HEAD
-
-let data = {
-	casesCompleted: 0,
-	total: {
-		startTimestamp: 0,
-		endTimestamp: 0
-	},
-	download: {
-		startTimestamp: 0,
-		endTimestamp: 0
-	},
-	unpacking: {
-		startTimestamp: 0,
-		endTimestamp: 0
-	},
-	parsing: {
-		startTimestamp: 0,
-		endTimestamp: 0
-	},
-	curcasetempdata: {
-		sid: undefined,
-		owMsg: undefined,
-		wasAlreadyConvicted: false,
-		aimbot_infractions: [],
-		AFKing_infractions: [],
-		Wallhack_infractions: [],
-		teamKill_infractions: [],
-		teamDamage_infractions: 0,
-		Reported: false
-	},
-	steamProfile: ""
-};
-
-let logonSettings = {
-	accountName: config.account.username,
-	password: config.account.password
-=======
 const coordinator = new Coordinator(steam, 730);
 let protobufs = undefined;
 let casesCompleted = 0;
@@ -70,7 +22,6 @@ let timings = {
 	Downloading: 0,
 	Unpacking: 0,
 	Parsing: 0
->>>>>>> 48ffed3427bacb17ec69ebff96f82016804d351a
 };
 
 (async () => {
@@ -302,265 +253,6 @@ coordinator.on("receivedFromGC", async (msgType, payload) => {
 		if (!demoBuffer) {
 			return;
 		}
-<<<<<<< HEAD
-		data.curcasetempdata.sid = sid;
-
-		let r = request(caseUpdate.caseurl);
-		r.on("response", (res) => {
-			res.pipe(fs.createWriteStream("./demofile.bz2")).on("close", async () => {
-				data.download.endTimestamp = Date.now();
-
-				// Successfully downloaded, tell the GC about it!
-				console.log("Finished downloading " + caseUpdate.caseid + ", unpacking...");
-
-				await csgoUser.sendMessage(
-					730,
-					csgoUser.Protos.csgo.ECsgoGCMsg.k_EMsgGCCStrike15_v2_PlayerOverwatchCaseStatus,
-					{},
-					csgoUser.Protos.csgo.CMsgGCCStrike15_v2_PlayerOverwatchCaseStatus,
-					{
-						caseid: caseUpdate.caseid,
-						statusid: 1
-					},
-					undefined,
-					undefined,
-					30000
-				);
-
-				data.unpacking.startTimestamp = Date.now();
-
-				// Parse the demo
-				fs.createReadStream("./demofile.bz2").pipe(bz2()).pipe(fs.createWriteStream("./demofile.dem")).on("close", () => {
-					data.unpacking.endTimestamp = Date.now();
-
-					if (fs.existsSync("./demofile.bz2")) {
-						fs.unlinkSync("./demofile.bz2");
-					}
-
-					data.parsing.startTimestamp = Date.now();
-
-					console.log("Finished unpacking " + caseUpdate.caseid + ", parsing as suspect " + sid.getSteamID64() + "...");
-
-					fs.readFile("./demofile.dem", (err, buffer) => {
-						if (err) return console.error(err);
-
-						data.curcasetempdata.aimbot_infractions = [];
-						data.curcasetempdata.AFKing_infractions = [];
-						data.curcasetempdata.Wallhack_infractions = [];
-						data.curcasetempdata.teamKill_infractions = [];
-						data.curcasetempdata.teamDamage_infractions = 0;
-						data.curcasetempdata.wasAlreadyConvicted = false;
-
-						let lastProg = -1;
-						let playerIndex = -1;
-						const demoFile = new demofile.DemoFile();
-
-						demoFile.gameEvents.on("player_connect", getPlayerIndex);
-						demoFile.gameEvents.on("player_disconnect", getPlayerIndex);
-						demoFile.gameEvents.on("round_freeze_end", getPlayerIndex);
-
-						function getPlayerIndex() {
-							playerIndex = demoFile.players.map(p => p.steamId === "BOT" ? p.steamId : new SteamID(p.steamId).getSteamID64()).indexOf(sid.getSteamID64());
-						}
-
-						demoFile.on("tickend", (curTick) => {
-							demoFile.emit("tickend__", { curTick: curTick, player: playerIndex });
-						});
-
-						// Detection
-						Aimbot(demoFile, sid, data, config);
-						AFKing(demoFile, sid, data, config);
-						Wallhack(demoFile, sid, data, config);
-						TeamKill(demoFile, sid, data);
-						TeamDamage(demoFile, sid, data);
-
-						demoFile.on("progress", (progressFraction) => {
-							let prog = Math.round(progressFraction * 100);
-							if (prog % 10 !== 0) {
-								return;
-							}
-
-							if (prog === lastProg) {
-								return;
-							}
-
-							lastProg = prog;
-							console.log("Parsing demo: " + prog + "%");
-						});
-
-						demoFile.parse(buffer);
-
-						demoFile.on("end", async (err) => {
-							data.parsing.endTimestamp = Date.now();
-
-							if (err.error) {
-								console.error(err);
-							}
-
-							console.log("Done parsing case " + caseUpdate.caseid);
-
-							// Setup conviction object
-							let convictionObj = {
-								caseid: caseUpdate.caseid,
-								suspectid: caseUpdate.suspectid,
-								fractionid: caseUpdate.fractionid,
-								rpt_aimbot: (data.curcasetempdata.aimbot_infractions.length > config.verdict.maxAimbot) ? 1 : 0,
-								rpt_wallhack: (data.curcasetempdata.Wallhack_infractions.length > config.verdict.maxWallKills) ? 1 : 0, // TODO: Add detection for looking at enemies through walls
-								rpt_speedhack: 0, // TODO: Add detection for other cheats (Ex BunnyHopping)
-								rpt_teamharm:  (data.curcasetempdata.teamDamage_infractions > config.verdict.maxTeamDamage ||  data.curcasetempdata.AFKing_infractions.length > config.verdict.maxAFKing
-									|| data.curcasetempdata.teamKill_infractions.length > config.verdict.maxTeamKills) ? 1 : 0, // TODO: Add detection for damaging teammates
-								reason: 3
-							};
-
-							if ((data.parsing.endTimestamp - data.parsing.startTimestamp) < (config.parsing.minimumTime * 1000)) {
-								// Wait this long before sending the request, if we parse the demo too fast the GC ignores us
-								let timer = parseInt((config.parsing.minimumTime * 1000) - (data.parsing.endTimestamp - data.parsing.startTimestamp)) / 1000;
-
-								console.log("Waiting " + timer + " second" + (timer === 1 ? "" : "s") + " to avoid the GC ignoring us");
-
-								await new Promise(r => setTimeout(r, (timer * 1000)));
-							}
-
-							// Check the Steam Web API, if a token is provided, if the user is already banned, if so always send a conviction even if the bot didn't detect it
-							if (config.parsing.steamWebAPIKey && config.parsing.steamWebAPIKey.length >= 10) {
-								let banChecker = await new Promise((resolve, reject) => {
-									request("https://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=" + config.parsing.steamWebAPIKey + "&format=json&steamids=" + sid.getSteamID64(), (err, res, body) => {
-										if (err) {
-											reject(err);
-											return;
-										}
-
-										let json = undefined;
-										try {
-											json = JSON.parse(body);
-										} catch (e) { };
-
-										if (json === undefined) {
-											reject(body);
-											return;
-										}
-
-										if (!json.players || json.players.length <= 0) {
-											reject(json);
-											return;
-										}
-
-										resolve(json.players[0]);
-									});
-								}).catch((err) => {
-									console.error(err);
-								});
-
-								if (banChecker && banChecker.NumberOfGameBans >= 1 && banChecker.DaysSinceLastBan <= 7 /* Demos are availble for 1 week */) {
-									// If the bot didn't catch the suspect aimbotting it is most likely just a waller and nothing else
-									convictionObj.rpt_wallhack = 1;
-
-									console.log("Suspect is already banned. Forcefully convicting...");
-
-									data.curcasetempdata.wasAlreadyConvicted = true;
-								} else {
-									console.log("Suspect has not been banned yet according to the Steam API");
-
-									data.curcasetempdata.wasAlreadyConvicted = false;
-								}
-							}
-
-							if(convictionObj.rpt_aimbot || convictionObj.rpt_wallhack || convictionObj.rpt_speedhack || convictionObj.rpt_teamharm) {
-								data.curcasetempdata.Reported = true;
-							} else {
-								data.curcasetempdata.Reported = false;
-							}
-
-							// Once we finished analysing the demo send the results
-							let caseUpdate2 = await csgoUser.sendMessage(
-								730,
-								csgoUser.Protos.csgo.ECsgoGCMsg.k_EMsgGCCStrike15_v2_PlayerOverwatchCaseUpdate,
-								{},
-								csgoUser.Protos.csgo.CMsgGCCStrike15_v2_PlayerOverwatchCaseUpdate,
-								convictionObj,
-								csgoUser.Protos.csgo.ECsgoGCMsg.k_EMsgGCCStrike15_v2_PlayerOverwatchCaseAssignment,
-								csgoUser.Protos.csgo.CMsgGCCStrike15_v2_PlayerOverwatchCaseAssignment,
-								30000
-							);
-
-							if (caseUpdate2.caseurl) {
-								// We got a new case despite sending a completion... Should never happen
-								console.log("Unexpected behaviour: Got a new case but sent convcitionObj. Retrying in 30 seconds");
-								setTimeout(doOverwatchCase, (30 * 1000));
-								return;
-							}
-
-							if (!caseUpdate2.caseid) {
-								console.log("Unexpected behaviour: Got a cooldown despite sending completion. Retrying in 30 seconds");
-								setTimeout(doOverwatchCase, (30 * 1000));
-								return;
-							}
-
-							data.total.endTimestamp = Date.now();
-							data.casesCompleted++;
-							data.curcasetempdata.steamProfile = "https://steamcommunity.com/profiles/"+ sid.getSteamID64() + "/";
-
-							// Print logs
-							console.log("Internal ID: " + data.casesCompleted);
-							console.log("CaseID: " + caseUpdate2.caseid);
-							console.log("Suspect: " + (data.curcasetempdata.sid ? data.curcasetempdata.sid.getSteamID64() : 0));
-							console.log("Infractions:");
-							console.log("	Aimbot: " + data.curcasetempdata.aimbot_infractions.length);
-							console.log("	Wallhack: " + data.curcasetempdata.Wallhack_infractions.length);
-							console.log("	TeamKills: " + data.curcasetempdata.teamKill_infractions.length);
-							console.log("	TeamDamage: " + data.curcasetempdata.teamDamage_infractions);
-							console.log("	Other: 0");
-							console.log("	Griefing: " + data.curcasetempdata.AFKing_infractions.length);
-							console.log("	Reported: " +(data.curcasetempdata.Reported ? "Yes" : "No"));
-							console.log("	Already convicted: " +(data.curcasetempdata.wasAlreadyConvicted ? "Yes" : "No"));
-							console.log("Timings:");
-							console.log("	Total: " + parseInt((data.total.endTimestamp - data.total.startTimestamp) / 1000) + "s");
-							console.log("	Download: " + parseInt((data.download.endTimestamp - data.download.startTimestamp) / 1000) + "s");
-							console.log("	Unpacking: " + parseInt((data.unpacking.endTimestamp - data.unpacking.startTimestamp) / 1000) + "s");
-							console.log("	Parsing: " + parseInt((data.parsing.endTimestamp - data.parsing.startTimestamp) / 1000) + "s");
-							console.log("	Throttle: " + caseUpdate2.throttleseconds + "s");
-
-							if (config.verdict.writeLog) {
-								if (!fs.existsSync("./cases")) {
-									fs.mkdirSync("./cases");
-								}
-
-								if (!fs.existsSync("./cases/" + caseUpdate2.caseid)) {
-									fs.mkdirSync("./cases/" + caseUpdate2.caseid);
-								}
-
-								// Write case file
-								fs.writeFileSync("./cases/" + caseUpdate2.caseid + "/message.json", JSON.stringify(data.curcasetempdata.owMsg, null, 4));
-								fs.writeFileSync("./cases/" + caseUpdate2.caseid + "/data.json", JSON.stringify(data, null, 4));
-							}
-
-							if (config.verdict.backupDemo) {
-								if (!fs.existsSync("./cases")) {
-									fs.mkdirSync("./cases");
-								}
-
-								if (!fs.existsSync("./cases/" + caseUpdate2.caseid)) {
-									fs.mkdirSync("./cases/" + caseUpdate2.caseid);
-								}
-
-								// Copy demo
-								fs.copyFileSync("./demofile.dem", "./cases/" + caseUpdate2.caseid + "/demofile.dem");
-							}
-
-							// Check case limit
-							if (config.verdict.maxVerdicts > 0 && data.casesCompleted >= config.verdict.maxVerdicts) {
-								console.log("Finished doing " + config.verdict.maxVerdicts + " case" + (config.verdict.maxVerdicts === 1 ? "" : "s"));
-								steamUser.logOff();
-								return;
-							}
-
-							// Request a overwatch case after the time has run out
-							setTimeout(doOverwatchCase, ((caseUpdate2.throttleseconds + 1) * 1000));
-						});
-					});
-				});
-			});
-=======
 
 		timings.Unpacking = Date.now() - timings.Unpacking;
 
@@ -611,7 +303,6 @@ coordinator.on("receivedFromGC", async (msgType, payload) => {
 
 			process.stdout.write("\r\x1b[K"); // Clear current line
 			process.stdout.write("Parsing: " + percentage + "%");
->>>>>>> 48ffed3427bacb17ec69ebff96f82016804d351a
 		});
 
 		timings.Parsing = Date.now();
