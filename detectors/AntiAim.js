8b4b6f4982fab9de4d27f0ba52ecff8bc24e1940
Black_Yuzia <25010528+BlackYuzia@users.noreply.github.com>
2020-10-04 20:11:54 +0300

    add one more check for yaw angles
    This check remove some false-positive detections.

--- detectors/AntiAim.js
+++ detectors/AntiAim.js
@@ -53,15 +53,18 @@ module.exports = class AntiAim {
 		// ! this detector can say what he have AntiAim (AA)... need more tests!
 		// ? Idea: https://www.unknowncheats.me/forum/counterstrike-global-offensive/208735-detecting-player-antiaim.html
 		const m_flLowerBodyYawTarget = this.parent.suspectPlayer.getProp("DT_CSPlayer", "m_flLowerBodyYawTarget");
-		const lbyDelta = m_flLowerBodyYawTarget - this.parent.suspectPlayer.eyeAngles.yaw;
-		if (lbyDelta <= 40 || this.parent.suspectPlayer.eyeAngles.pitch !== 0) {
+		const eyeAngles = this.parent.suspectPlayer.eyeAngles;
+		const lbyDelta = m_flLowerBodyYawTarget - eyeAngles.yaw;
+		if (lbyDelta <= 40 || eyeAngles.pitch !== 0 || eyeAngles.yaw !== 0) {
+			// ? if I didn't check yaw I get false positive sometimes. 
+			// ? but in rage cheater what really have AA detects didn't increase or decrease
 			// All good
 			return;
 		}
 
 		this.infractions.push({
 			tick: this.parent.demo.currentTick,
-			angles: this.parent.suspectPlayer.eyeAngles,
+			angles: eyeAngles,
 			lowerBodyYaw: m_flLowerBodyYawTarget,
 			lowerBodyDelta: lbyDelta
 		});
