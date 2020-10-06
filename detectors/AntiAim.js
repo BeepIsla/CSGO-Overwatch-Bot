7df1f61604e4606d8c0df5e247384831ab3073c3
Black_Yuzia <25010528+BlackYuzia@users.noreply.github.com>
2020-10-06 20:19:18 +0300

    Update AntiAim.js

--- detectors/AntiAim.js
+++ detectors/AntiAim.js
@@ -42,31 +42,35 @@ module.exports = class AntiAim {
 	 * Custom Methods *
 	 ******************/
 	OnTickEnd(tick) {
-		if (!this.parent.suspectPlayer || !this.parent.suspectPlayer.isAlive) {
-			// Suspect left or is dead
+		if (!this.parent.suspectPlayer || !this.parent.suspectPlayer.isAlive || this.parent.demo.gameRules.isWarmup) {
+			// Suspect left or is dead or warmup
+			return;
+		}
+		const cheatAngle = 88.99;
+		// Angles are hardcoded
+		// Currently only one angle I have. 
+		// ! Need more tests and more angles (?)
+		// ? Note: If that method wouldn't work success for >= 30% of cheaters (with AA) we should delete this.
+		const { pitch, yaw } = this.parent.suspectPlayer.eyeAngles;
+
+		const lookAtDown = (value) => parseFloat(value.toFixed(2), 10) === cheatAngle;
+		// const lookAtUp = (value) => parseFloat(value.toFixed(2), 10) === -88.99;
+		// ? Note: Cheaters have AA what look at up but I didn't test it so I disable this check. I need more demos.
+
+
+		if (!lookAtDown(pitch)) {
+			// Player or Cheater did't look at floor (down)
 			return;
 		}
 
-		// Check if player look at floor and check angles
-		// ! Be aware. This detector is unstable. 
-		// ! I don't sure by 100%, but if player just look at floor (0deg) and didn't kill anybody
-		// ! this detector can say what he have AntiAim (AA)... need more tests!
-		// ? Idea: https://www.unknowncheats.me/forum/counterstrike-global-offensive/208735-detecting-player-antiaim.html
-		const m_flLowerBodyYawTarget = this.parent.suspectPlayer.getProp("DT_CSPlayer", "m_flLowerBodyYawTarget");
-		const eyeAngles = this.parent.suspectPlayer.eyeAngles;
-		const lbyDelta = m_flLowerBodyYawTarget - eyeAngles.yaw;
-		if (lbyDelta <= 40 || eyeAngles.pitch !== 0 || eyeAngles.yaw !== 0) {
-			// ? if I didn't check yaw I get false positive sometimes. 
-			// ? but in rage cheater what really have AA detects didn't increase or decrease
-			// All good
-			return;
-		}
-
-		this.infractions.push({
-			tick: this.parent.demo.currentTick,
-			angles: eyeAngles,
-			lowerBodyYaw: m_flLowerBodyYawTarget,
-			lowerBodyDelta: lbyDelta
-		});
+		this.infractions.push(
+			{
+				tick: tick,
+				currTick: this.parent.demo.currentTick,
+				angles: { pitch, yaw },
+				isLookAtDown: lookAtDown(pitch),
+				// isLookAtUp: lookAtUp(eyeAngles.pitch)
+			}
+		);
 	}
 };
