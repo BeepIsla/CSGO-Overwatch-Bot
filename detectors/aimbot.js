cff0d0263a3901fba2e9e827eaf6bbb17798b2a4
Isla <BeepIsla@gmail.com>
2021-06-23 19:55:46 +0200

    Make aimbot detector better

--- detectors/aimbot.js
+++ detectors/aimbot.js
@@ -1,5 +1,3 @@
-const almostEqual = require("almost-equal");
-
 // This class name MUST be unique or it will override other results
 module.exports = class Aimbot {
 	constructor(parent, config) {
@@ -64,33 +62,16 @@ module.exports = class Aimbot {
 		}
 
 		for (let i = 0; i < this.lastAngles.length; i++) {
-			// Check pitch
-			if (typeof this.lastAngles[i] !== "undefined" && typeof this.lastAngles[i + 1] !== "undefined") {
-				if (!almostEqual(this.lastAngles[i].pitch, this.lastAngles[i + 1].pitch, this.config.parsing.aimbot.threshold)) {
-					if (this.Is360Difference(this.lastAngles[i].pitch, this.lastAngles[i + 1].pitch)) {
-						continue;
+			if (this.lastAngles[i] && this.lastAngles[i + 1]) {
+				for (let ang of ["yaw", "pitch"]) {
+					let distance = this.CalculateDifference(this.lastAngles[i][ang], this.lastAngles[i + 1][ang]);
+					if (distance > this.config.parsing.aimbot.threshold) {
+						this.infractions.push({
+							prevAngle: this.lastAngles[i],
+							nextAngle: this.lastAngles[i + 1],
+							tick: this.parent.demo.currentTick
+						});
 					}
-
-					this.infractions.push({
-						prevAngle: this.lastAngles[i],
-						nextAngle: this.lastAngles[i + 1],
-						tick: this.parent.demo.currentTick
-					});
-				}
-			}
-
-			// Check yaw
-			if (typeof this.lastAngles[i] !== "undefined" && typeof this.lastAngles[i + 1] !== "undefined") {
-				if (!almostEqual(this.lastAngles[i].yaw, this.lastAngles[i + 1].yaw, this.config.parsing.aimbot.threshold)) {
-					if (this.Is360Difference(this.lastAngles[i].yaw, this.lastAngles[i + 1].yaw)) {
-						continue;
-					}
-
-					this.infractions.push({
-						prevAngle: this.lastAngles[i],
-						nextAngle: this.lastAngles[i + 1],
-						tick: this.parent.demo.currentTick
-					});
 				}
 			}
 		}
@@ -99,18 +80,10 @@ module.exports = class Aimbot {
 	/*************************************
 	 * Helpers specific to this detector *
 	 *************************************/
-	// Shitty check for 360 changes
-	Is360Difference(angle1, angle2) {
-		// Check 0 < 360
-		if (angle1 <= this.config.parsing.aimbot.threshold && angle1 >= 0.0 && angle2 <= 360.0 && angle2 >= (360.0 - this.config.parsing.aimbot.threshold)) {
-			return true;
-		}
-
-		// Check 360 > 0
-		if (angle1 <= 360.0 && angle1 >= (360.0 - this.config.parsing.aimbot.threshold) && angle2 <= this.config.parsing.aimbot.threshold && angle2 >= 0.0) {
-			return true;
-		}
-
-		return false;
+	CalculateDifference(a, b) {
+		let diff = b - a;
+		if (diff < -180) diff += 360;
+		if (diff > 180) diff -= 360;
+		return diff < 0 ? -diff : diff;
 	}
 };
